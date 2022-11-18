309ff13e0cf217f706d33a6f73c010e4e6644d6f
FatemehLavasi <111527593+Fatemehlavasi@users.noreply.github.com>
2022-11-18 05:01:56 -0800

    add singel page

--- src/pages/singlePage.js
+++ src/pages/singlePage.js
@@ -1,14 +1,18 @@
 import WeatherFutureCard from "../component/weatherFutureCard";
-import {useParams} from "react-router-dom";
-import {useEffect, useState} from "react";
-import {handleGetFiveDayWeather} from "../api/api";
-import {Grid} from "@mui/material";
-
-const daysIndex = [0, 8, 16, 24, 32]
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { handleGetFiveDayWeather } from "../api/api";
+import { Button, Grid, Typography, Container } from "@mui/material";
+import { Link } from "react-router-dom";
+import Header from "../component/Header";
+import DeleteIcon from '@mui/icons-material/Delete';
+import "../component/Styles.css"
+import LocationCityIcon from '@mui/icons-material/LocationCity';
+const daysIndex = [0, 8, 16, 24]
 
 const SinglePage = () => {
     const [cityWeatherFuture, setCityWeatherFuture] = useState({})
-    const {city} = useParams()
+    const { city } = useParams()
 
     const handleGetData = async () => {
         const data = await handleGetFiveDayWeather(city)
@@ -20,13 +24,26 @@ const SinglePage = () => {
     }, [])
 
     return (
-        <Grid>
-            <Grid>
-                {city}
-            </Grid>
-            <Grid>
-                {daysIndex.map(index => <WeatherFutureCard key={index} index={index}
-                                                           cityWeatherFuture={cityWeatherFuture}/>)}
+        <Grid container item dir={'rtl'} alignItems={'center'} justifyContent={'center'} md={12} flexDirection={"column"} >
+            <Grid bgcolor={"red"} mt={"5"} className={"hero1"}>
+                <Header />
+                <Grid container item md={12} bgcolor={"#000"} className={"opacity"} flexDirection={"column"} mt={5}>
+                    <Grid item>
+                        <Typography variant="h1" color={"#fff"} pr={3} >{city}</Typography>
+                    </Grid>
+                    <Grid item >
+                        <Typography variant="h2" p={3} color={"#fff"}>{" وضعیت آب و هوا در 4 روز آینده  "}</Typography>
+                    </Grid>
+                    <Grid xs={12} p={3} mt={5}  >
+                        <Link to={"/"}>
+                            <Typography color={"#fff"} border={1} p={2} borderRadius={2} textAlign={"center"}>  {"بازگشت به صفحه اصلی"} </Typography>
+                        </Link>
+                    </Grid>
+                </Grid>
+                <Grid item md={12} display={"flex"} justifyContent={"space-around"} flexWrap={"wrap"}>
+                    {daysIndex.map(index => <WeatherFutureCard key={index} index={index}
+                        cityWeatherFuture={cityWeatherFuture} />)}
+                </Grid>
             </Grid>
         </Grid>
     )
