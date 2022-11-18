e81c521ff346b915731a5a368d943d372405a23d
FatemehLavasi <111527593+Fatemehlavasi@users.noreply.github.com>
2022-11-18 05:02:40 -0800

    add page home

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,29 +1,44 @@
-import { Grid } from "@mui/material";
+import { Grid, Container, CircularProgress } from "@mui/material";
 import WeatherCard from "../component/weatherCard";
 import { useEffect, useState } from "react";
 import { handleGetOneDayWeather } from "../api/api";
 import SelectCity from "../component/SelectCity";
 import Header from "../component/Header";
+import img from "../images/image.jpeg"
+import "../component/Styles.css"
 
 const Home = () => {
+    const [loading, setLoading] = useState(true)
     const [city, setCity] = useState('tehran');
     const [cityWeather, setCityWeather] = useState({})
 
     const handleGetData = async () => {
         const data = await handleGetOneDayWeather(city)
         setCityWeather(data)
+        setLoading(false)
     }
 
     useEffect(() => {
         handleGetData()
     }, [city])
-
+    if (loading) {
+        return <Grid container item xs={12} alignItems={'center'} justifyContent={'center'}>
+            <CircularProgress />
+        </Grid>
+    }
     return (
-        <Grid container alignItems={'center'} justifyContent={'center'} flexDirection={'column'} p={2} dir={'rtl'} height={"100vh"}>
-            <Grid item height={"600px"} p={1} width={'1200px'} bgcolor={"#1A3670"}>
-                <Header />
-                <SelectCity currency={city} setCurrency={setCity} />
-                <WeatherCard cityWeather={cityWeather} city={city} />
+        <Grid container alignItems={'center'} justifyContent={'center'} mt={5} flexDirection={'column'}  dir={'rtl'} >
+            <Grid border={1} borderRadius={2} container item md={8} xs={12}  p={2} className={"hero"}>
+            {/* <img width={"70px"} src={img} alt="jmbghgffxdgh" /> */}
+                {/* <Container> */}
+                    <Header />
+                    {/* <Grid mt={5} bgcolor={"yellowgreen"} item display={"flex"} flexDirection={'column'} > */}
+                        <SelectCity currency={city} setCurrency={setCity} cityWeather={cityWeather} />
+                        <WeatherCard cityWeather={cityWeather} city={city} />
+                    {/* </Grid> */}
+                    {/* </Grid> */}
+                {/* </Container > */}
+
             </Grid>
         </Grid>
     )
